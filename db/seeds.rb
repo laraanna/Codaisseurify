ed937e587460d25e8c2b4f6d0a4fe7f0b3a33a31
laraanna <lara@laraannawagner.com>
2017-11-11 22:36:04 +0100

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Artist.destroy_all
 Song.destroy_all
 
 #artists
-adrian = Artist.create!(
+artist_1 = Artist.create!(
   name: "Adrian Ström" ,
   genre: "Dance/Electro" ,
   bio: "Creates music and captures moments of life with his camera.
@@ -10,7 +10,7 @@ adrian = Artist.create!(
   remote_image_url: "http://res.cloudinary.com/dtvsqvonc/image/upload/v1510389935/adrian_rndige.jpg"
 )
 
-bilderbuch = Artist.create!(
+artist_2 = Artist.create!(
   name: "Bilderbuch" ,
   genre: "Rock" ,
   bio: "Bilderbuch is an Austrian rock band, formed in Kremsmünster, Austria in 2005.
@@ -19,7 +19,7 @@ bilderbuch = Artist.create!(
   remote_image_url: "http://res.cloudinary.com/dtvsqvonc/image/upload/v1510389935/bilderbuch_wrmrj3.jpg"
 )
 
-billy = Artist.create!(
+artist_3 = Artist.create!(
   name: "Billy Joel" ,
   genre: "Rock music, Rock and roll, Soft rock, Pop music" ,
   bio: "William Martin Joel is an American singer-songwriter and pianist.
@@ -28,7 +28,7 @@ billy = Artist.create!(
   remote_image_url: "http://res.cloudinary.com/dtvsqvonc/image/upload/v1510389935/billy_tigrfd.jpg"
 )
 
-amine = Artist.create!(
+artist_4 = Artist.create!(
   name: "Aminé" ,
   genre: "Alternative Hip Hop",
   bio: "Adam Aminé Daniel, professionally known as Aminé, is an American rapper.
@@ -36,19 +36,61 @@ amine = Artist.create!(
   remote_image_url: "http://res.cloudinary.com/dtvsqvonc/image/upload/v1510389935/amine_ehxgxa.jpg"
 )
 
+artist_5 = Artist.create!(
+  name: "Paul Kalkbrenner" ,
+  genre: "Techno/Electronic",
+  bio: "Paul Kalkbrenner is a German live act, producer of electronic music, and actor from Berlin.
+  Because he breaks down his tracks into elements that are reassembled onstage, Kalkbrenner is considered a live act,
+  as opposed to a DJ.He is most known for his single 'Sky and Sand', which sold over 200,000 copies, went platinum, and was
+  highly charted in countries such as Belgium and Germany.",
+  remote_image_url: "http://res.cloudinary.com/dtvsqvonc/image/upload/v1510435059/paul_k2ukdr.jpg"
+)
+
+artist_6 = Artist.create!(
+  name: "Möwe" ,
+  genre: "Dance/electronic, Metal",
+  bio: "Mel&Clem are the creators of MÖWE.
+  This charismatic Viennese duo could not have expected that something which sprouted from
+  boredom on January 10th 2013 would lead to far more. But let´s start at the beginning:
+  Mel is a tattooed and cheeky rebel, was a member of many Indie-Rock bands previously,
+  including Mary lost her Pathos. Clemens, a charming, young heartbreaker was also involved with bands previously,
+  including Mary Lost Her Pathos as the group’s bass player.",
+  remote_image_url: "http://res.cloudinary.com/dtvsqvonc/image/upload/v1510433870/Moewe_ymbjcm.jpg"
+)
+
+
+
+artist_7 = Artist.create!(
+  name: "Moby" ,
+  genre: "Dance",
+  bio: "One of electronic music's most visible and talked-about figures,
+  Moby's career comprises many years of work and many musical twists and turns.
+  His career stretches all the way back to early rave and acid-house releases in the late 80s and early 90s as U.H.F.
+  and Voodoo Child, through to his much loved/hated R&B/blues/pop/techno sample cut-ups of today. ",
+  remote_image_url: "http://res.cloudinary.com/dtvsqvonc/image/upload/v1510433871/Moby_fuoc98.jpg"
+)
+
 
 
 #songs
 
-amour = Song.create!( title: "Pensée sur l'amour", year_of_release:2016, artist: adrian )
-somewhere_inside = Song.create!( title: "Somewhere Inside", year_of_release:2016, artist: adrian )
+song_1 = Song.create!( title: "Pensée sur l'amour", year_of_release:2016, artist: artist_1 )
+song_2 = Song.create!( title: "Somewhere Inside", year_of_release:2016, artist: artist_1 )
 
-bungalow = Song.create!( title: "Bungalow", year_of_release:2017, artist: bilderbuch )
-baba = Song.create!( title: "Baba", year_of_release:2017, artist: bilderbuch )
+song_3 = Song.create!( title: "Bungalow", year_of_release:2017, artist: artist_2 )
+song_4 = Song.create!( title: "Baba", year_of_release:2017, artist: artist_2 )
 
-piano_man = Song.create!( title: "Piano Man", year_of_release:1973, artist: billy )
-uptown_girl = Song.create!( title: "Uptown Girl", year_of_release:1983, artist: billy )
-vienna = Song.create!( title: "Vienna", year_of_release:1977, artist: billy )
+song_5 = Song.create!( title: "Piano Man", year_of_release:1973, artist: artist_3 )
+song_6 = Song.create!( title: "Uptown Girl", year_of_release:1983, artist: artist_3 )
+song_7 = Song.create!( title: "Vienna", year_of_release:1977, artist: artist_3 )
 
-caroline = Song.create!( title: "caroline", year_of_release:2017, artist: amine )
-broccoli = Song.create!( title: "Broccoli", year_of_release:2015, artist: amine )
+song_8 = Song.create!( title: "caroline", year_of_release:2017, artist: artist_4 )
+song_9 = Song.create!( title: "Broccoli", year_of_release:2015, artist: artist_4 )
+
+song_10 = Song.create!( title: "Feed Your Head", year_of_release:2015, artist: artist_5 )
+song_11 = Song.create!( title: "Sky And Sand", year_of_release:2008, artist: artist_5 )
+
+song_12 = Song.create!( title: "Back In The Summer", year_of_release:2016, artist: artist_6 )
+
+song_13 = Song.create!( title: "In My Heart", year_of_release:2002, artist: artist_7 )
+song_14 = Song.create!( title: "Why Does My Heart Feel So Bad?", year_of_release:1999, artist: artist_7 )
