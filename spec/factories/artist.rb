e10d9e5d75ba5dd7e213ff58ce001b69b7470230
laraanna <lara@laraannawagner.com>
2017-11-12 11:40:22 +0100

    Feature Test Artist View Page

--- spec/factories/artist.rb
+++ spec/factories/artist.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :artist do
-    name      { Faker::Lorem.sentence(1) }
-    genre     { Faker::Lorem.sentence(1) }
-    bio       { Faker::Lorem.sentence(40) }
+    name                { Faker::Lorem.sentence(1) }
+    genre               { Faker::Lorem.sentence(1) }
+    bio                 { Faker::Lorem.sentence(40) }
+    remote_image_url    { Faker::Avatar.image }
   end
 end
